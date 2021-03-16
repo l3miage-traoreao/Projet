c54ee71ad08bf936c1cbbbc8db4216c97c08f132
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-03-16 13:25:33 +0100

    Ajouté d'autres graphes pour les autres dates

--- main.R
+++ main.R
@@ -4,3 +4,17 @@ ggplot(aes(x=region, y=couv_tot_dose1), data = nbVaccineUltramarineEtMetropolita
   geom_bar(stat = "identity") +
   labs(x="Region", y="% de Vaccinées")
 
+# Graphe du pourcentage des personnes vaccinées dans tous les regions (01-03-21)
+ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_01_20h15) +
+  geom_bar(stat = "identity") +
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (01-03-21)")
+
+# Graphe du pourcentage des personnes vaccinées dans tous les regions (08-03-21)
+ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_08_17h25) +
+  geom_bar(stat = "identity") +
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (08-03-21)")
+
+# Graphe du pourcentage des personnes vaccinées dans tous les regions (15-03-21)
+ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_15_17h25) +
+  geom_bar(stat = "identity") +
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (15-03-21)")
