223443ddeda7ba362c6cc8fa90879a24fb2c27f8
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-03-22 16:38:11 +0100

    Donnee du 18-03 ajoutee

--- main.R
+++ main.R
@@ -3,18 +3,28 @@
 ggplot(aes(x=region, y=couv_tot_dose1), data = nbVaccineUltramarineEtMetropolitaine) +
   geom_bar(stat = "identity") +
   labs(x="Region", y="% de Vaccinées")
+  ylim(0,0.1)
 
 # Graphe du pourcentage des personnes vaccinées dans tous les regions (01-03-21)
-ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_01_20h15) +
+ggplot(aes(x=as.factor(reg), y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_01_20h15) +
   geom_bar(stat = "identity") +
-  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (01-03-21)")
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (01-03-21)") +
+  ylim(0,12)
 
 # Graphe du pourcentage des personnes vaccinées dans tous les regions (08-03-21)
 ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_08_17h25) +
   geom_bar(stat = "identity") +
-  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (08-03-21)")
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (08-03-21)") +
+  ylim(0,12)
 
 # Graphe du pourcentage des personnes vaccinées dans tous les regions (15-03-21)
 ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_15_17h25) +
   geom_bar(stat = "identity") +
-  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (15-03-21)")
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (15-03-21)") +
+  ylim(0,12)
+
+# Graphe du pourcentage des personnes vaccinées dans tous les regions (18-03-21)
+ggplot(aes(x=reg, y=couv_tot_dose1), data = vacsi_tot_reg_2021_03_18_17h25) +
+  geom_bar(stat = "identity") +
+  labs(x="Region", y="% de Vaccinées", title = "Pourcentage des Personnes Vaccinées dans tous les Regions (18-03-21)") +
+  ylim(0,12)
